3d2ef2c1d53c9d97bc825d83e81b25df749506b7
zhuangqiang <zhuangqiang@asiainfo.com>
2020-04-05 10:05:33 +0800

    webpack test

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -4,7 +4,7 @@ const assert = require('assert');
 describe('webpack.base.js test case', () => {
   const baseConfig = require('../../lib/webpack.base')
   it('entry', () => {
-    assert.equal(baseConfig.entry.index, '/Users/zq/Documents/learnCode/GeekTime/02-玩转webpack/build-webpack/test/smoke/template/src/index/index.js')
-    assert.equal(baseConfig.entry.search, '/Users/zq/Documents/learnCode/GeekTime/02-玩转webpack/build-webpack/test/smoke/template/src/search/index.js')
+    assert.equal(baseConfig.entry.index.indexOf('test/smoke/template/src/index/index.js') > -1 , true)
+    assert.equal(baseConfig.entry.search.indexOf('test/smoke/template/src/search/index.js') > -1 , true)
   })
  })
